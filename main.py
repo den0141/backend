955d49a1e9a991fdc051b768b9a6403cc36899aa
den0141 <glebchernih02@gmail.com>
2026-03-26 15:28:51 +0500

    Refactor CORS and change login endpoint to GET
    Updated CORS settings and modified login endpoint to use GET method.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -6,38 +5,29 @@ import requests
 
 app = FastAPI()
 
-# =======================
-#   CORS — разрешаем GitHub Pages
-# =======================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://den0141.github.io"],  # ссылка на твой WebApp
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# =======================
-#   Модель запроса
-# =======================
 class LoginRequest(BaseModel):
     initData: str
 
-# =======================
-#   Эндпоинт логина
-# =======================
 @app.post("/api/login")
 async def login(req: LoginRequest):
-    # Эмуляция запроса к игре (заменить на настоящий API игры)
     GAME_URL = "https://oldprison-prod.luckygem.online/api/auth/login"
 
     payload = {"initData": req.initData}
 
+    game_response = requests.get(GAME_URL, params=payload)
+
     try:
-        game_response = requests.post(GAME_URL, json=payload, timeout=10)
         return game_response.json()
     except:
-        return {"success": False, "detail": "Ошибка авторизации в игре"}
+        return {"error": True, "detail": "game response invalid"}
 
 @app.get("/")
 async def root():
